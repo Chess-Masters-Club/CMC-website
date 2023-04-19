102b362e446f8340041fa48ae6d00fde9726c1d7
rd07 <rd07gamer@gmail.com>
2023-04-19 15:25:14 +0100

    added the title shared component

---
+++ cmc-website/src/components/shared/Title.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Bishop from "../../../public/W_Bishop.png";
+import king from "../../../public/W_King.png";
+import Knight from "../../../public/W_Knight.png";
+
+import "./Title.css";
+
+export default function Title({ title, image }) {
+  return (
+    <div className="flex mb-10">
+      <img
+        className="lg:w-14 md:w-12 w-10 mr-5 object-contain h-auto"
+        src={image}
+      />
+      <h1 className=" mt-12 text-white text-[40px] md:text-[45px] lg:text-[50px]">
+        {title}
+      </h1>
+    </div>
+  );
+}
