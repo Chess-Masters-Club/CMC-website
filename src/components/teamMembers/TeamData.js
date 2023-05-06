9aa6db4b6dc716c5ff24521c1c31058b3d2cac76
rd07 <rd07gamer@gmail.com>
2023-05-06 11:45:08 +0100

    minor changes to fix deployment

--- src/components/teamMembers/TeamData.js
+++ src/components/teamMembers/TeamData.js
@@ -1,234 +1,234 @@
-import Bishop from '../../../public/W_Bishop.png';
-import King from '../../../public/W_King.png';
-import Knight from '../../../public/W_Knight.png';
-import Pawn from '../../../public/W_Pawn.png';
-import Queen from '../../../public/W_Queen.png';
-import Rook from '../../../public/W_Rook.png';
-import tou from '../../../public/Team/1.jpg';
-import a2 from '../../../public/Team/2.jpg';
-import a3 from '../../../public/Team/3.jpg';
-import a4 from '../../../public/Team/4.jpg';
-import a5 from '../../../public/Team/5.jpg';
-import a6 from '../../../public/Team/6.png';
-import a7 from '../../../public/Team/7.jpg';
-import a8 from '../../../public/Team/8.jpg';
-import a9 from '../../../public/Team/9.jpg';
-import a10 from '../../../public/Team/10.jpg';
-import a12 from '../../../public/Team/12.jpg';
-import a13 from '../../../public/Team/13_2.jpg';
-import nadhir from '../../../public/Team/14.jpg';
-import a15 from '../../../public/Team/15.jpg';
-import a17 from '../../../public/Team/17.jpg';
-import a43 from '../../../public/Team/43.jpg';
-import a55 from '../../../public/Team/55.jpg';
-import AnisNedjah from '../../../public/Team/AnisNedjah.jpg';
-import AouatiZakaria from '../../../public/Team/AouatiZakaria.jpg';
-import BelkisHanchi from '../../../public/Team/BelkisHanchi.jpg';
-import IhsaneKerroum from '../../../public/Team/IhsaneKerroum.jpg';
-import InesBenlatreche from '../../../public/Team/InesBenlatreche.jpg';
-import KhaledOussamaabdelwadjed from '../../../public/Team/KhaledOussamaabdelwadjed.jpg';
-import MedAmineTouati from '../../../public/Team/MedAmineTouati.jpg';
-import user from '../../../public/Team/user.png';
-import MohamedAli from '../../../public/Team/MohamedAli.jpg';
-import RayanAg from '../../../public/Team/RayanAg.jpg';
-import Saraaidi from '../../../public/Team/Saraaidi.jpg';
-import YacineBoukaaba from '../../../public/Team/YacineBoukaaba.jpg';
-import YounesIslamKaouani from '../../../public/Team/Younes IslamKaouani.jpg';
-import yousramimouni from '../../../public/Team/yousramimouni.jpg';
-import a45 from '../../../public/Team/45.jpg';
-import inferno from '../../../public/Team/inferno.jpg';
+import Bishop from "../../../public/W_Bishop.png";
+import King from "../../../public/W_King.png";
+import Knight from "../../../public/W_Knight.png";
+import Pawn from "../../../public/W_Pawn.png";
+import Queen from "../../../public/W_Queen.png";
+import Rook from "../../../public/W_Rook.png";
+import tou from "../../../public/Team/1.jpg";
+import a2 from "../../../public/Team/2.jpg";
+import a3 from "../../../public/Team/3.jpg";
+import a4 from "../../../public/Team/4.jpg";
+import a5 from "../../../public/Team/5.jpg";
+import a6 from "../../../public/Team/6.png";
+import a7 from "../../../public/Team/7.jpg";
+import a8 from "../../../public/Team/8.jpg";
+import a9 from "../../../public/Team/9.jpg";
+import a10 from "../../../public/Team/10.jpg";
+import a12 from "../../../public/Team/12.jpg";
+import a13 from "../../../public/Team/13_2.jpg";
+import nadhir from "../../../public/Team/14.jpg";
+import a15 from "../../../public/Team/15.jpg";
+import a17 from "../../../public/Team/17.jpg";
+import a43 from "../../../public/Team/43.jpg";
+import a55 from "../../../public/Team/55.jpg";
+import AnisNedjah from "../../../public/Team/AnisNedjah.jpg";
+import AouatiZakaria from "../../../public/Team/AouatiZakaria.jpg";
+import BelkisHanchi from "../../../public/Team/BelkisHanchi.jpg";
+import IhsaneKerroum from "../../../public/Team/IhsaneKerroum.jpg";
+import InesBenlatreche from "../../../public/Team/InesBenlatreche.jpg";
+import KhaledOussamaabdelwadjed from "../../../public/Team/KhaledOussamaabdelwadjed.jpg";
+import MedAmineTouati from "../../../public/Team/MedAmineTouati.jpg";
+import user from "../../../public/Team/user.png";
+import MohamedAli from "../../../public/Team/MohamedAli.jpg";
+import RayanAg from "../../../public/Team/RayanAg.jpg";
+import Saraaidi from "../../../public/Team/Saraaidi.jpg";
+import YacineBoukaaba from "../../../public/Team/YacineBoukaaba.jpg";
+import YounesIslamKaouani from "../../../public/Team/Younes IslamKaouani.jpg";
+import yousramimouni from "../../../public/Team/yousramimouni.jpg";
+import a45 from "../../../public/Team/45.jpg";
+import inferno from "../../../public/Team/Inferno.jpg";
 
 const P = "President";
 const TM = "Team member";
-const TL = 'Team leader';
+const TL = "Team leader";
 export const president = {
-    name: 'Toufik Menaa',
-    image: tou,
-    role: P
+  name: "Toufik Menaa",
+  image: tou,
+  role: P,
 };
 export const teamMembers = [
-    {
-        name: 'Nadhir Djabali',
-        image: nadhir,
-        role: TM,
-    },
-    {
-        name: 'Raid Teyar',
-        image: a12,
-        role: TM,
-    },
-    {
-        name: 'Lekhal rahma',
-        image: a7,
-        role: TM,
-    },
-    {
-        name: 'Soudani Fadoua',
-        image: a55,
-        role: TM,
-    },
-    {
-        name: 'Cherouana Amina',
-        image: a8,
-        role: TM,
-    },
+  {
+    name: "Nadhir Djabali",
+    image: nadhir,
+    role: TM,
+  },
+  {
+    name: "Raid Teyar",
+    image: a12,
+    role: TM,
+  },
+  {
+    name: "Lekhal rahma",
+    image: a7,
+    role: TM,
+  },
+  {
+    name: "Soudani Fadoua",
+    image: a55,
+    role: TM,
+  },
+  {
+    name: "Cherouana Amina",
+    image: a8,
+    role: TM,
+  },
 
-    {
-        name: 'Chidekh Malak',
-        image: a10,
-        role: TM,
-    },
-    {
-        name: 'Ihsane Kerroum',
-        image: IhsaneKerroum,
-        role: TM,
-    },
-    {
-        name: 'Mohamed ali Inouri',
-        image: MohamedAli,
-        role: TM,
-    },
-    {
-        name: 'Touati Mohamed Amine',
-        image: MedAmineTouati,
-        role: TM,
-    },
-    {
-        name: 'Khaled Oussama',
-        image: KhaledOussamaabdelwadjed,
-        role: TM,
-    },
-    {
-        name: 'Younes kaouani',
-        image: YounesIslamKaouani,
-        role: TM,
-    },
-    {
-        name: 'Bareche Radouane',
-        image: a43,
-        role: TM,
-    },
-    {
-        name: 'Anisse Nedjah',
-        image: AnisNedjah,
-        role: TM,
-    },
-    {
-        name: 'Mimouni Yousra',
-        image: yousramimouni,
-        role: TM,
-    },
-    {
-        name: 'Hanchi belkis',
-        image: BelkisHanchi,
-        role: TM,
-    },
-    {
-        name: 'Kerouaz louey',
-        image: a13,
-        role: TM,
-    },
-    {
-        name: 'Boucenina Seif',
-        image: a15,
-        role: TM,
-    },
-    {
-        name: 'Hadjaz Mohamed Islam',
-        image: a17,
-        role: TM,
-    },
-    {
-        name: 'Aidi Sara',
-        image: Saraaidi,
-        role: TM,
-    },
-    {
-        name: 'Mohamed Khalouche',
-        image: inferno,
-        role: TM,
-    },
-    {
-        name: 'Agabi Rayan',
-        image: RayanAg,
-        role: TM,
-    },
-    {
-        name: 'Ines Benlatreche',
-        image: InesBenlatreche,
-        role: TM,
-    },
-    {
-        name: 'Yacine boukaba ',
-        image: YacineBoukaaba,
-        role: TM,
-    },
-    {
-        name: 'Aouati zakaria',
-        image: AouatiZakaria,
-        role: TM,
-    },
-    {
-        name: 'Islam Kadri',
-        image: a45,
-        role: TM,
-    },
-    {
-        name: 'Mellal Ahmed',
-        image: user,
-        role: TM,
-    },
+  {
+    name: "Chidekh Malak",
+    image: a10,
+    role: TM,
+  },
+  {
+    name: "Ihsane Kerroum",
+    image: IhsaneKerroum,
+    role: TM,
+  },
+  {
+    name: "Mohamed ali Inouri",
+    image: MohamedAli,
+    role: TM,
+  },
+  {
+    name: "Touati Mohamed Amine",
+    image: MedAmineTouati,
+    role: TM,
+  },
+  {
+    name: "Khaled Oussama",
+    image: KhaledOussamaabdelwadjed,
+    role: TM,
+  },
+  {
+    name: "Younes kaouani",
+    image: YounesIslamKaouani,
+    role: TM,
+  },
+  {
+    name: "Bareche Radouane",
+    image: a43,
+    role: TM,
+  },
+  {
+    name: "Anisse Nedjah",
+    image: AnisNedjah,
+    role: TM,
+  },
+  {
+    name: "Mimouni Yousra",
+    image: yousramimouni,
+    role: TM,
+  },
+  {
+    name: "Hanchi belkis",
+    image: BelkisHanchi,
+    role: TM,
+  },
+  {
+    name: "Kerouaz louey",
+    image: a13,
+    role: TM,
+  },
+  {
+    name: "Boucenina Seif",
+    image: a15,
+    role: TM,
+  },
+  {
+    name: "Hadjaz Mohamed Islam",
+    image: a17,
+    role: TM,
+  },
+  {
+    name: "Aidi Sara",
+    image: Saraaidi,
+    role: TM,
+  },
+  {
+    name: "Mohamed Khalouche",
+    image: inferno,
+    role: TM,
+  },
+  {
+    name: "Agabi Rayan",
+    image: RayanAg,
+    role: TM,
+  },
+  {
+    name: "Ines Benlatreche",
+    image: InesBenlatreche,
+    role: TM,
+  },
+  {
+    name: "Yacine boukaba ",
+    image: YacineBoukaaba,
+    role: TM,
+  },
+  {
+    name: "Aouati zakaria",
+    image: AouatiZakaria,
+    role: TM,
+  },
+  {
+    name: "Islam Kadri",
+    image: a45,
+    role: TM,
+  },
+  {
+    name: "Mellal Ahmed",
+    image: user,
+    role: TM,
+  },
 
-    {
-        name: 'Mouad Rouabah',
-        image: user,
-        role: TM,
-    },
-    {
-        name: 'Redouane Zeghba',
-        image: user,
-        role: TM,
-    },
-    {
-        name: 'Houssem Magra',
-        image: user,
-        role: TM,
-    },
-    {
-        name: 'Bousbia yazid',
-        image: a9,
-        role: TM,
-    },
+  {
+    name: "Mouad Rouabah",
+    image: user,
+    role: TM,
+  },
+  {
+    name: "Redouane Zeghba",
+    image: user,
+    role: TM,
+  },
+  {
+    name: "Houssem Magra",
+    image: user,
+    role: TM,
+  },
+  {
+    name: "Bousbia yazid",
+    image: a9,
+    role: TM,
+  },
 ];
 export const leaders = [
-    {
-        name: 'younes boudjada',
-        image: a2,
-        role: 'Media ' + TL
-    },
-    {
-        name: 'Bakhouche akram',
-        image: a6,
-        role: 'ER ' + TL,
-    },
-    {
-        name: 'Kassa Yasmine ',
-        image: a5,
-        role: 'IR ' + TL,
-    },
-    {
-        name: 'DAHIAS Ali Zineddine',
-        image: a3,
-        role: 'Tech ' + TL
-    },
-    {
-        name: 'Mallem Bourhane',
-        image: user,
-        role: 'F&L ' + TL
-    },
-    {
-        name: 'Mahboub brahim ',
-        image: a4,
-        role: 'Design ' + TL
-    },
+  {
+    name: "younes boudjada",
+    image: a2,
+    role: "Media " + TL,
+  },
+  {
+    name: "Bakhouche akram",
+    image: a6,
+    role: "ER " + TL,
+  },
+  {
+    name: "Kassa Yasmine ",
+    image: a5,
+    role: "IR " + TL,
+  },
+  {
+    name: "DAHIAS Ali Zineddine",
+    image: a3,
+    role: "Tech " + TL,
+  },
+  {
+    name: "Mallem Bourhane",
+    image: user,
+    role: "F&L " + TL,
+  },
+  {
+    name: "Mahboub brahim ",
+    image: a4,
+    role: "Design " + TL,
+  },
 ];
